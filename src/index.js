08bc4175d4aa4ec04ecfd220649892bdf0272031
jkhbuild <jkh.build@gmail.com>
2022-11-12 14:51:28 -0500

    trade constant added

--- src/index.js
+++ src/index.js
@@ -1,18 +1,41 @@
 import Example from "./scripts/example";
 import Chart from "./scripts/chart";
 import Data from "./scripts/office.json";
-// const Chart = require("./scripts/chart");
+
+// variable to store all x axis (construction trades)
+const tradeNames = [
+  "Gen Reqs",
+  "Demo",
+  "Conc",
+  "Mas",
+  "Mtls",
+  "Woods",
+  "Therm Prot",
+  "Wndws",
+  "Fins",
+  "Spec",
+  "Furn",
+  "Spec Const",
+  "Conv Sys",
+  "Mech/Plbg",
+  "Elec",
+];
 
 // user input event listener / set input variables
-document.getElementById("user-input").addEventListener("submit", createBudget);
+let form = document.getElementById("user-input");
+if (form) form.addEventListener("submit", createBudget);
 
 // user input event handler
 function createBudget(e) {
   e.preventDefault();
 
-  const squareFootage = document.getElementsById("sf");
-  const budget = document.getElementsById("budget");
+  const squareFootage = document.getElementsById("sf").value;
+  const budget = document.getElementsById("budget").value;
+  console.log(squareFootage);
+  console.log(budget);
 }
+
+//
 
 // document.addEventListener("DOMContentLoaded", () => {
 //   const main = document.getElementById("main");
