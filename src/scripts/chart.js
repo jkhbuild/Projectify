65478b889486cb667a6b27b50e8076c103d41f07
jkhbuild <jkh.build@gmail.com>
2022-11-13 13:17:25 -0500

    functions and constants moved into event handler

--- src/scripts/chart.js
+++ src/scripts/chart.js
@@ -22,49 +22,40 @@ const tradeNames = [
 let form = document.getElementById("user-input");
 if (form) form.addEventListener("submit", createBudget);
 
-let squareFootage;
 // user input event handler
 function createBudget(e) {
   e.preventDefault();
 
-  // squareFootage = document.getElementById("sf").value;
-  squareFootage = 100;
+  const squareFootage = document.getElementById("sf").value;
   const budget = document.getElementById("budget").value;
+
+  // y-Axis values
+  const yAxis = Object.values(Data.rates);
+  for (let i = 0; i < yAxis.length; i++) {
+    yAxis[i] *= squareFootage;
+  }
+
+  // x-Axis values
+  const xAxis = Object.keys(Data.rates);
+
+  // Creating SVG
+  let svg = d3
+    .select("#main-chart")
+    .append("svg")
+    .attr("width", 500)
+    .attr("height", 800)
+    .attr("transform", "translate(" + 100 + "," + 100 + ")");
+
+  // Creating y-Axis
+  let y = d3.scaleLinear().range([500, 0]);
+
+  function createAxisLeft(data) {
+    y.domain([0, d3.max(yAxis)]).nice();
+
+    svg.append("g").call(d3.axisLeft(y));
+  }
+  console.log(yAxis);
   createAxisLeft(Data);
 
-  // console.log(maxHeight(rates, squareFootage));
-}
-
-// rates data from JSON
-const rates = Data.rates;
-
-// rates keys stored for x Axis
-const xAxis = Object.keys(Data.rates);
-
-// rates values stored in an array
-const yAxis = Object.values(Data.rates);
-console.log(yAxis);
-
-// rates * squarefootage
-for (let i = 0; i < yAxis.length; i++) {
-  yAxis[i] *= squareFootage;
-}
-console.log(yAxis);
-
-// Creating SVG
-let svg = d3
-  .select("#main-chart")
-  .append("svg")
-  .attr("width", 500)
-  .attr("height", 800)
-  .attr("transform", "translate(" + 100 + "," + 100 + ")");
-
-// Creating Bar Chart
-let y = d3.scaleLinear().range([500, 0]);
-
-function createAxisLeft(data) {
-  y.domain([0, d3.max(yAxis)]).nice();
-
-  svg.append("g").call(d3.axisLeft(y));
-  console.log(yAxis);
+  //Creating x-Axis
 }
