e50bb5bb22511eccb6d0dc1670b02104bd1fb42e
jkhbuild <jkh.build@gmail.com>
2022-11-13 00:13:02 -0500

    y-axis created

--- src/index.js
+++ src/index.js
@@ -1,38 +1,8 @@
 import Example from "./scripts/example";
-import Chart from "./scripts/chart";
+import "./scripts/chart";
 import Data from "./scripts/office.json";
 
 // variable to store all x axis (construction trades)
-const tradeNames = [
-  "Gen Reqs",
-  "Demo",
-  "Conc",
-  "Mas",
-  "Mtls",
-  "Woods",
-  "Therm Prot",
-  "Wndws",
-  "Fins",
-  "Spec",
-  "Furn",
-  "Spec Const",
-  "Conv Sys",
-  "Mech/Plbg",
-  "Elec",
-];
-
-// user input event listener / set input variables
-let form = document.getElementById("user-input");
-if (form) form.addEventListener("submit", createBudget);
-
-// user input event handler
-function createBudget(e) {
-  e.preventDefault();
-
-  const squareFootage = document.getElementById("sf").value;
-  const budget = document.getElementById("budget").value;
-}
-
 //
 
 // document.addEventListener("DOMContentLoaded", () => {
--- src/scripts/chart.js
+++ src/scripts/chart.js
@@ -1,3 +1,5 @@
+import Data from "./office.json";
+// import * as d3 from "d3";
 const tradeNames = [
   "Gen Reqs",
   "Demo",
@@ -20,12 +22,52 @@ const tradeNames = [
 let form = document.getElementById("user-input");
 if (form) form.addEventListener("submit", createBudget);
 
+let squareFootage;
 // user input event handler
 function createBudget(e) {
   e.preventDefault();
 
-  const squareFootage = document.getElementById("sf").value;
+  squareFootage = document.getElementById("sf").value;
   const budget = document.getElementById("budget").value;
+  createAxisLeft(Data);
+
+  // console.log(maxHeight(rates, squareFootage));
 }
 
-module.exports = Chart;
+// rates data from JSON
+const rates = Data.rates;
+
+// rates keys stored for x Axis
+const xAxis = Object.keys(Data.rates);
+
+// rates values stored in an array
+const yAxis = Object.values(Data.rates);
+console.log(yAxis);
+
+// rates * squarefootage
+for (let i = 0; i < yAxis.length; i++) {
+  console.log(Number(yAxis[i]));
+  console.log(typeof yAxis[i]);
+  console.log(Number(squareFootage));
+  console.log(typeof squareFootage);
+  yAxis[i] = parseInt(yAxis[i]) * parseInt(squareFootage);
+}
+console.log(yAxis);
+
+// Creating SVG
+let svg = d3
+  .select("#main-chart")
+  .append("svg")
+  .attr("width", 500)
+  .attr("height", 800)
+  .attr("transform", "translate(" + 100 + "," + 100 + ")");
+
+// Creating Bar Chart
+let y = d3.scaleLinear().range([500, 0]);
+
+function createAxisLeft(data) {
+  y.domain([0, d3.max(yAxis)]).nice();
+
+  svg.append("g").call(d3.axisLeft(y));
+  console.log(yAxis);
+}
