b35786ba355ca489d9af352ce42332bec8cbe44b
jkhbuild <jkh.build@gmail.com>
2022-11-15 11:58:56 -0500

    chart class created, squarefootage implemented

--- src/index.js
+++ src/index.js
@@ -1,57 +1,14 @@
 // import "./scripts/chartrf.js";
 import "./scripts/tradeselect.js";
 import Data from "./scripts/office.json";
-// import { _ } from "core-js";
+import Chart from "./scripts/chart.js";
 
-const width = 800;
+const width = 850;
 const height = 750;
 const margin = { top: 50, bottom: 50, left: 50, right: 50 };
 
-const svg = d3
-  .select("#main-chart")
-  .append("svg")
-  .attr("height", height - margin.top - margin.bottom)
-  .attr("width", width - margin.left - margin.right)
-  .attr("viewBox", [0, 0, width, height]);
-
-const x = d3
-  .scaleBand()
-  .domain(d3.range(Data.length))
-  .range([margin.left, width - margin.right])
-  .padding(0.1);
-
-const y = d3
-  .scaleLinear()
-  .domain([0, d3.max(Data.map((e) => e.rate))])
-  .range([height - margin.bottom, margin.top]);
-
-svg
-  .append("g")
-  .attr("fill", "royalblue")
-  .selectAll("rect")
-  .data(Data)
-  .join("rect")
-  .attr("x", (d, i) => x(i))
-  .attr("y", (d) => y(d.rate))
-  .attr("height", (d) => y(0) - y(d.rate))
-  .attr("width", x.bandwidth());
-
-function xAxis(g) {
-  // g.attr("transform", "translate(0, ${height - margin.bottom})");
-  g.attr("transform", `translate(0, ${height - margin.bottom})`)
-    .call(d3.axisBottom(x).tickFormat((i) => Data[i].trade))
-    .attr("font-size", "12px");
-}
-
-function yAxis(g) {
-  g.attr("transform", `translate(${margin.left}, 0)`)
-    .call(d3.axisLeft(y).ticks(null, Data.format))
-    .attr("font-size", "16px");
-}
-
-svg.append("g").call(yAxis);
-svg.append("g").call(xAxis);
-svg.node();
+let chart = new Chart(width, height, margin);
+chart.createChart(Data);
 
 // user input event listener / set input variables
 let form = document.getElementById("user-input");
@@ -63,3 +20,5 @@ function createBudget(e) {
   const squareFootage = document.getElementById("sf").value;
   const budget = document.getElementById("budget").value;
 }
+
+console.log(Data);
--- src/scripts/chart.js
+++ src/scripts/chart.js
@@ -1,68 +1,69 @@
-import Data from "./scripts/office.json";
+import Data from "./office.json";
+// const width = 800;
+// const height = 750;
+// const margin = { top: 50, bottom: 50, left: 50, right: 50 };
 
-class Chart{
-  constructor(data, xlabels) {
+class Chart {
+  constructor(width, height, margin) {
+    this.width = width;
+    this.height = height;
+    this.margin = margin;
+  }
 
+  createChart(data, sf = 0) {
+    for (let i = 0; i < data.length; i++) {
+      data[i].rate *= sf;
+    }
+
+    console.log(data);
+
+    this.svg = d3
+      .select("#main-chart")
+      .append("svg")
+      .attr("height", this.height - this.margin.top - this.margin.bottom)
+      .attr("width", this.width - this.margin.left - this.margin.right)
+      .attr("viewBox", [0, 0, this.width, this.height]);
+
+    this.x = d3
+      .scaleBand()
+      .domain(d3.range(data.length))
+      .range([this.margin.left, this.width - this.margin.right])
+      .padding(0.1);
+
+    this.y = d3
+      .scaleLinear()
+      .domain([0, d3.max(Data.map((e) => e.rate))])
+      .range([this.height - this.margin.bottom, this.margin.top]);
+
+    this.svg
+      .append("g")
+      .attr("fill", "royalblue")
+      .selectAll("rect")
+      .data(data)
+      .join("rect")
+      .attr("x", (d, i) => this.x(i))
+      .attr("y", (d) => this.y(d.rate))
+      .attr("height", (d) => this.y(0) - this.y(d.rate))
+      .attr("width", this.x.bandwidth());
+
+    function xAxis(g) {
+      g.attr("transform", `translate(0, ${this.height - this.margin.bottom})`)
+        .call(d3.axisBottom(this.x).tickFormat((i) => data[i].trade))
+        .attr("font-size", "12px");
+    }
+    console.log(this.margin);
+    console.log(this.margin.left);
+    function yAxis(g) {
+      console.log(g);
+      g.attr("transform", `translate(${this.margin.left}, 0)`)
+        .call(d3.axisLeft(this.y).ticks(null, data.format))
+        .attr("font-size", "12px");
+    }
+
+    this.svg.append("g").call(xAxis.bind(this));
+    this.svg.append("g").call(yAxis.bind(this));
+    this.svg.node();
   }
 }
-  const width = 800;
-  const height = 750;
-  const margin = { top: 50, bottom: 50, left: 50, right: 50 };
 
-  const svg = d3
-    .select("#main-chart")
-    .append("svg")
-    .attr("height", height - margin.top - margin.bottom)
-    .attr("width", width - margin.left - margin.right)
-    .attr("viewBox", [0, 0, width, height]);
-
-  const x = d3
-    .scaleBand()
-    .domain(d3.range(Data.length))
-    .range([margin.left, width - margin.right])
-    .padding(0.1);
-
-  const y = d3
-    .scaleLinear()
-    .domain([0, 100])
-    .range([height - margin.bottom, margin.top]);
-
-  svg
-    .append("g")
-    .attr("fill", "royalblue")
-    .selectAll("rect")
-    .data(Data)
-    .join("rect")
-    .attr("x", (d, i) => x(i))
-    .attr("y", (d) => y(d.rate))
-    .attr("height", (d) => y(0) - y(d.rate))
-    .attr("width", x.bandwidth());
-
-  function xAxis(g) {
-    // g.attr("transform", "translate(0, ${height - margin.bottom})");
-    g.attr("transform", `translate(0, ${height - margin.bottom})`)
-      .call(d3.axisBottom(x).tickFormat((i) => Data[i].trade))
-      .attr("font-size", "12px");
-  }
-
-  function yAxis(g) {
-    g.attr("transform", `translate(${margin.left}, 0)`)
-      .call(d3.axisLeft(y).ticks(null, Data.format))
-      .attr("font-size", "12px");
-  }
-
-  svg.append("g").call(yAxis);
-  svg.append("g").call(xAxis);
-  svg.node();
-
-  // user input event listener / set input variables
-  let form = document.getElementById("user-input");
-  if (form) form.addEventListener("submit", createBudget);
-
-  // user input event handler
-  function createBudget(e) {
-    e.preventDefault();
-    const squareFootage = document.getElementById("sf").value;
-    const budget = document.getElementById("budget").value;
-  }
-}
+export default Chart;
