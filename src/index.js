1ad318d09d70c3b62a857dbafaa9bb69fbbebe15
jkhbuild <jkh.build@gmail.com>
2022-11-15 18:07:32 -0500

    x axis removal functionality added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // import "./scripts/chartrf.js";
-import "./scripts/tradeselect.js";
+// import "./scripts/tradeselect.js";
 import Data from "./scripts/office.json";
 import Data2 from "./scripts/office.json";
 import Chart from "./scripts/chart.js";
@@ -13,19 +13,52 @@ const margin = { top: 50, bottom: 50, left: 50, right: 50 };
 
 let chart = new Chart(width, height, margin);
 chart.createChart(dup);
-
+dup = dupData(Data);
 // user input event listener / set input variables
 let form = document.getElementById("user-input");
 if (form) form.addEventListener("submit", createBudget);
-
+let squareFootage;
 // user input event handler
 function createBudget(e) {
   e.preventDefault();
-  const squareFootage = document.getElementById("sf").value;
+  squareFootage = document.getElementById("sf").value;
   const budget = document.getElementById("budget").value;
 
   chart.deleteChart();
   let dup = dupData(Data);
   chart.createChart(dup, squareFootage);
-  console.log(chart.getTotal(dup));
+}
+
+const tradesExcludedDiv = document.getElementById("trades-excluded");
+const tradesSelectedDiv = document.getElementById("trade-selector");
+const excluded = [];
+
+if (tradesSelectedDiv) {
+  tradesSelectedDiv.addEventListener("click", excludeTrade);
+}
+
+function excludeTrade(e) {
+  let clicked = e.target;
+  if (clicked.className === "trade") {
+    tradesExcludedDiv.appendChild(e.target);
+  }
+  for (let i = 0; i < dup.length; i++) {
+    if (dup[i].trade === clicked.id) {
+      dup = dup.splice(0, i).concat(dup.splice(i + 1));
+    }
+  }
+  console.log(Data);
+  console.log(dup);
+  chart.deleteChart();
+  chart.createChart(dup, squareFootage);
+}
+
+let tradesExcluded = document.getElementById("trades-excluded");
+if (tradesExcluded) tradesExcluded.addEventListener("click", includeTrade);
+
+function includeTrade(e) {
+  let clicked = e.target;
+  if (clicked.className === "trade") {
+    tradesSelectedDiv.appendChild(e.target);
+  }
 }
--- src/scripts/tradeselect.js
+++ src/scripts/tradeselect.js
@@ -1,15 +1,30 @@
+import dupData from "./data";
+import Data from "./office.json";
+import Chart from "./chart.js";
+
+// chart = new Chart();
 const tradesExcludedDiv = document.getElementById("trades-excluded");
 const tradesSelectedDiv = document.getElementById("trade-selector");
+const excluded = [];
 
-if (tradesSelectedDiv)
+if (tradesSelectedDiv) {
   tradesSelectedDiv.addEventListener("click", excludeTrade);
+}
 
 function excludeTrade(e) {
   let clicked = e.target;
-  console.log(clicked.className);
   if (clicked.className === "trade") {
     tradesExcludedDiv.appendChild(e.target);
   }
+  let dup = dupData(Data);
+  for (let i = 0; i < dup.length; i++) {
+    if (dup[i].trade === clicked.id) {
+      dup = dup.splice(0, i).concat(dup.splice(i + 1));
+      console.log(dup);
+    }
+  }
+  chart.deleteChart();
+  chart.createChart(dup, squareFootage);
 }
 
 let tradesExcluded = document.getElementById("trades-excluded");
@@ -22,3 +37,5 @@ function includeTrade(e) {
     tradesSelectedDiv.appendChild(e.target);
   }
 }
+
+export default userinput;
