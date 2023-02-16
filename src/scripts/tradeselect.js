85a54519af02f67300fda188ef13e65bf15af56f
jkhbuild <jkh.build@gmail.com>
2023-02-15 20:23:33 -0500

    remove commented out code from tradeselect

--- src/scripts/tradeselect.js
+++ src/scripts/tradeselect.js
@@ -2,49 +2,6 @@ import dupData from "./data";
 import Data from "./office.json";
 import Chart from "./chart.js";
 
-// chart = new Chart();
-// const tradesExcludedDiv = document.getElementById("trades-excluded");
-// const tradesSelectedDiv = document.getElementById("trade-selector");
-// const excluded = [];
-
-// if (tradesSelectedDiv) {
-//   tradesSelectedDiv.addEventListener("click", excludeTrade);
-// }
-
-// function excludeTrade(e) {
-//   let clicked = e.target;
-//   if (clicked.className === "trade") {
-//     tradesExcludedDiv.appendChild(e.target);
-//   }
-//   let dup = dupData(Data);
-//   for (let i = 0; i < dup.length; i++) {
-//     if (dup[i].trade === clicked.id) {
-//       dup = dup.splice(0, i).concat(dup.splice(i + 1));
-//       console.log(dup);
-//     }
-//   }
-//   chart.deleteChart();
-//   chart.createChart(dup, squareFootage);
-// }
-
-// let tradesExcluded = document.getElementById("trades-excluded");
-// if (tradesExcluded) tradesExcluded.addEventListener("click", includeTrade);
-
-// function includeTrade(e) {
-//   let clicked = e.target;
-//   console.log(clicked.classList);
-//   if (clicked.className === "trade") {
-//     tradesSelectedDiv.appendChild(e.target);
-//   }
-// }
-
-// const tradesExcludedDiv = document.getElementById("trades-excluded");
-// const tradesSelectedDiv = document.getElementById("trade-selector");
-// let excluded = [];
-
-// if (tradesSelectedDiv) {
-//   tradesSelectedDiv.addEventListener("click", excludeTrade);
-// }
 function excludeTrade(e) {
   let clicked = e.target;
   if (clicked.className === "trade") {
@@ -64,8 +21,6 @@ function excludeTrade(e) {
   }
 }
 
-// let tradesExcluded = document.getElementById("trades-excluded");
-// if (tradesExcluded) tradesExcluded.addEventListener("click", includeTrade);
 function includeTrade(e) {
   let clicked = e.target;
   if (clicked.className === "trade") {
@@ -81,7 +36,6 @@ function includeTrade(e) {
   }
 }
 
-// moves all excluded trades back into included.
 function resetTrades() {
   for (let i = 0; i < excluded.length; i++) {
     move = document.getElementById(excluded[i].trade);
